8615bb69089ef02e4ca4c4fa6b56027cd98aba5c
Katya Weicht <katya.weicht@gmail.com>
2026-04-24 16:12:43 -0700

    Add methods to console view class

--- ConsoleView.java
+++ ConsoleView.java
@@ -1,6 +1,77 @@
-public class ConsoleView{ //add implements GameView
+import java.util.List;
+import java.util.Scanner;
 
-  public static void main(String[] args){
+public class ConsoleView implements GameView{ //add implements GameView
+  private final Scanner in = new Scanner(System.in);
+
+  public ConsoleView(){
 
   }
+
+  public boolean checkContinue(){
+    System.out.println("Would you like to continue on your quest (y/n)? ");
+    if(in.nextLine().equalsIgnoreCase("y") || in.nextLine().equalsIgnoreCase("yes")){
+      return true;
+    }
+    return false;
+  }
+
+  public String displayMainMenu(){
+    System.out.println("What would you like to do?");
+    return in.nextLine();
+  }
+
+  public void endGame(){
+    System.out.println("Thank you for playing!");
+  }
+
+  public void knightNotFound(){
+    System.out.println("Knight not found!");
+  }
+
+  public void listKnights(List<Knight> knights){
+    if(knights.isEmpty()){
+      System.out.println("No knights to list");
+    }
+    else {
+      int i = 1;
+      for(Knight knight: knights){
+        System.out.println(i + ": " + knight.getName());
+        i += 1;
+      }
+    }
+  }
+
+  public void printBattleText(List<MOB> monsters, List<Knight> activeKnights){
+    
+  }
+
+  public void printBattleText(MOB dead){
+
+  }
+
+  public void printDefeated(){
+
+  }
+
+  public void printFortunes(List<Knight> activeKnights){
+
+  }
+
+  public void printHelp(){
+
+  }
+
+  public void setActiveFailed(){
+
+  }
+
+  public void showKnight(Knight knight){
+
+  }
+
+  public void splashScreen(){
+    
+  }
+  
 }
