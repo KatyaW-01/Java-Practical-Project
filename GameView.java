2ad410fcb8270f1af20c8725d3438bc77e2bd1dd
Katya Weicht <katya.weicht@gmail.com>
2026-04-24 14:56:58 -0700

    Create game view interface

--- GameView.java
+++ GameView.java
@@ -1,6 +1,17 @@
-public interface GameView{
-  
-  public static void main(String[] args){
+import java.util.List;
 
-  }
+public interface GameView{
+  boolean checkContinue();
+  String displayMainMenu();
+  void endGame();
+  void knightNotFound();
+  void listKnights(List<Knight> knights);
+  void printBattleText(List<MOB> monsters, List<Knight> activeKnights);
+  void printBattleText(MOB dead);
+  void printDefeated();
+  void printFortunes(List<Knight> activeKnights);
+  void printHelp();
+  void setActiveFailed();
+  void showKnight(Knight knight);
+  void splashScreen();
 }
