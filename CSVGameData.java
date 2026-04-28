596271c6f58b88e2d9961c5b03f08bca3dfb0d70
Katya Weicht <katya.weicht@gmail.com>
2026-04-27 17:42:46 -0700

    Create load gamedata and load savedata methods

--- CSVGameData.java
+++ CSVGameData.java
@@ -1,6 +1,71 @@
-public class CSVGameData { //add extends GameData
+import java.util.Scanner;
 
-  public static void main(String[] args){
+public class CSVGameData extends GameData {
+  public CSVGameData(String gameData, String saveData){
+    loadGameData(gameData);
+    loadSaveData(saveData);
+  }
+  
+  public void loadSaveData(String saveData){
+    int counter = 0;
+    Scanner file = readFile(saveData);
+    if(file == null) return;
+    while(file.hasNextLine()){
+      Scanner line = new Scanner(file.nextLine());
+      line.useDelimiter(",");
+      Knight kt = new Knight(
+        ++ counter,
+        line.next().trim(),
+        line.nextInt(),
+        line.nextInt(),
+        line.nextInt(),
+        DiceType.valueOf(line.next()),
+        line.nextInt()
+      );
+      knights.add(kt);
+    }
+  }
+
+  public void loadGameData(String gameData){
+    Scanner file = readFile(gameData);
+    if(file == null) return;
+    while(file.hasNextLine()){
+      Scanner line = new Scanner(file.nextLine());
+      line.useDelimiter(",");
+      if(line.next().trim().toLowerCase().equals("fortune")){
+        //create a fortune object here
+        Fortune ftn = new Fortune(
+          line.next().trim(),
+          line.nextInt(),
+          line.nextInt(),
+          line.nextInt(),
+          line.next().equals("-") ? null : DiceType.valueOf(line.next())
+        );
+        fortunes.add(ftn);
+      }
+      if(line.next().trim().toLowerCase().equals("mob")){
+        //create a MOB object here 
+        MOB monster = new MOB(
+          line.next().trim(),
+          line.nextInt(),
+          line.nextInt(),
+          line.nextInt(),
+          DiceType.valueOf(line.next())
+        );
+        monsters.add(monster);
+      }
+    }
+  }
+
+  private Scanner readFile(String fileName){
+    Scanner scnr = new Scanner(System.in);
+  }
+
+  private void parseGameDataLine(Scanner line){
+
+  }
+
+  public void save(String filename){
 
   }
 }
